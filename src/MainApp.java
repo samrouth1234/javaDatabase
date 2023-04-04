b415d605d3a7e787cdca840617e1b9c63dc02b5a
samrouth <bichsamroth@gmail.com>
2023-04-04 14:26:55 +0700

    This is a homework

--- src/MainApp.java
+++ src/MainApp.java
@@ -7,101 +7,99 @@ public class MainApp {
     private static Jdbclmpl jdb;
     private static Scanner scanner;
         public static void main(String[] args) {
-            jdb=new Jdbclmpl();
-            scanner=new Scanner(System.in);
-            int option;
-            Jdbclmpl jdb=new Jdbclmpl();
-            List<Topic> topics=new ArrayList<>();
+            jdb = new Jdbclmpl();
+            scanner = new Scanner(System.in);
             Topic topic=new Topic();
-
-           do {
-               System.out.println("======================================================================");
-                System.out.println("1.UPDATE OPERATION ");
-                System.out.println("2.DELETE BY ID OPERATION");
-                System.out.println("3.SELECT ID OPERATION");
-                System.out.println("4.SELECT BY NAME OPERATION");
-                System.out.println("5.EXIT");
-                System.out.println("======================================================================");
-                System.out.print("==> Chooses option 1->5 :");
-                option= scanner.nextInt();
-                switch (option) {
-                   case 1 -> {
-                       System.out.println("===============UPDATE OPERATION===============");
-                       System.out.print("Enter id :");
-                       topic.setId(scanner.nextInt());
-                       System.out.print("Enter name :");
-                       topic.setName(scanner.nextLine());
-                       scanner.nextLine();
-                       System.out.print("Enter Description :");
-                       topic.setDescription(scanner.nextLine());
-                       topic.setStatus(true);
-                       try (Connection conn = jdb.dataSource().getConnection()) {
-                           String sql = "UPDATE toys SET name=?, description=?,status=? WHERE id="+ topic.getId();
-                           PreparedStatement statement = conn.prepareStatement(sql);
-                           statement.setString(1, topic.getName());
-                           statement.setString(2, topic.getDescription());
-                           statement.setBoolean(3, topic.getStatus());
-                           statement.executeUpdate(sql);
-                           System.out.println("Updated Successfully!!");
-                       } catch (SQLException e) {
-                           e.printStackTrace();
-                       }
-                       scanner.nextInt();
-                   }
-                   case 2 -> {
-                       System.out.println("===============Delete By ID OPERATION===============");
-                       System.out.print("Enter id :");
-                       topic.setId(scanner.nextInt());
-                       try (Connection connection = jdb.dataSource().getConnection()) {
-                           String sql = "DELETE FROM toys WHERE id="+topic.getId();
-                           PreparedStatement statements = connection.prepareStatement(sql);
-                           System.out.println("Updated Successfully!!");
-                       } catch (SQLException e) {
-                           e.printStackTrace();
-                       }
-                   }
-                   case 3 -> {
-                       System.out.println("===============SELECT By ID OPERATION===============");
-                       System.out.print("Enter id :");
-                       topic.setId(scanner.nextInt());
-                       try (Connection connections = jdb.dataSource().getConnection()) {
-                           String sql = "SELECT * FROM toys WHERE id="+ topic.getId();
-                           PreparedStatement statement = connections.prepareStatement(sql);
-                           ResultSet resultSet =  statement.executeQuery();
-                           while (resultSet.next()){
-                               Integer id=resultSet.getInt("id");
-                               String name=resultSet.getString("name");
-                               String description=resultSet.getString("description");
-                               Boolean status=resultSet.getBoolean("status");
-                               topics.add(new Topic(id,name,description,status));
-                           }
-                           topics.forEach(System.out::println);
-                           } catch (SQLException e) {
-                           e.printStackTrace();
-                        }
-                   }
-                   case 4 -> {
-                       System.out.println("===============SELECT By NAME OPERATION===============");
-                       System.out.print("Enter name :");
-                       topic.setName(scanner.nextLine());
-                       try (Connection connecs = jdb.dataSource().getConnection()) {
-                           String sql = "SELECT * FROM toys WHERE name ILIKE '%"+ topic.getName()+"%'";
-                           PreparedStatement statements = connecs.prepareStatement(sql);
-                           ResultSet resultSet =  statements.executeQuery();
-                           while (resultSet.next()){
-                               Integer id = resultSet.getInt("id");
-                               String name = resultSet.getString("name");
-                               String description = resultSet.getString("description");
-                               boolean status = resultSet.getBoolean("status");
-                               topics.add(new Topic(id,name,description,status));
-                           }
-                           } catch (SQLException e) {
-                           e.printStackTrace();
-                       }
-                       scanner.nextInt();
-                   }
-                    default -> System.out.println("Chose try again ..!");
-               }
-            }while (option!=5);
+            System.out.print("Enter id :");
+            topic.setId(scanner.nextInt());
+            System.out.print("Enter name :");
+            scanner.nextLine();
+            topic.setName(scanner.nextLine());
+            System.out.print("Enter Description :");
+            topic.setDescription(scanner.nextLine());
+            topic.setStatus(true);
+            updateTopic(topic);
+            insertTopic(topic);
+            deleteById(topic);
+            selectByName(topic.getName());
+            selectDatabase();
         }
+        private static void selectDatabase(){
+            try (Connection con=jdb.dataSource().getConnection()){
+                String sql="SELECT * FROM topic";
+                PreparedStatement statement=con.prepareStatement(sql);
+                ResultSet resultSet=statement.executeQuery();
+                List<Topic>topics=new ArrayList<>();
+                while (resultSet.next()){
+                    Integer id=resultSet.getInt("id");
+                    String name=resultSet.getString("name");
+                    String description=resultSet.getString("description");
+                    Boolean status=resultSet.getBoolean("status");
+                    topics.add(new Topic(id,name,description,status));
+                }
+                topics.forEach(System.out::println);
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
+    private static void insertTopic(Topic topic) {
+        try (Connection con = jdb.dataSource().getConnection()) {
+            String insertSql = "INSERT INTO topic(name,description,status)" + "VALUES(?,?,?)";
+            PreparedStatement statement = con.prepareStatement(insertSql);
+            statement.setString(1, topic.getName());
+            statement.setString(2, topic.getDescription());
+            statement.setBoolean(3, topic.getStatus());
+            int count = statement.executeUpdate();
+            System.out.println(count);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    private static void updateTopic(Topic topic){
+        try (Connection con = jdb.dataSource().getConnection()) {
+            String updateDatabase = "UPDATE topic SET name=?,description=?,status=? WHERE id=?";
+            PreparedStatement statement = con.prepareStatement(updateDatabase);
+            statement.setString(1, topic.getName());
+            statement.setString(2, topic.getDescription());
+            statement.setBoolean(3, topic.getStatus());
+            statement.setInt(4, topic.getId());
+            int count = statement.executeUpdate();
+            System.out.println(count);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    private static void deleteById(Topic topic){
+        try (Connection con = jdb.dataSource().getConnection()) {
+            String deleteDatabase = "DELETE FROM topic WHERE id=?";
+            PreparedStatement statement = con.prepareStatement(deleteDatabase);
+            statement.setInt(1, topic.getId());
+            int count = statement.executeUpdate();
+            System.out.println(count);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    private static void selectById(Integer id){
+        try (Connection con = jdb.dataSource().getConnection()) {
+            String selectID = "SELECT FROM topic WHERE id=?";
+            PreparedStatement statement = con.prepareStatement(selectID);
+            statement.setInt(1,id);
+            ResultSet count = statement.executeQuery();
+            System.out.println(count);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    private static void selectByName(String name){
+        try (Connection con = jdb.dataSource().getConnection()) {
+            String selectName = "SELECT FROM topic WHERE name LIKE ?";
+            PreparedStatement statement = con.prepareStatement(selectName);
+            statement.setString(1,name);
+            ResultSet count = statement.executeQuery();
+            System.out.println(count);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
